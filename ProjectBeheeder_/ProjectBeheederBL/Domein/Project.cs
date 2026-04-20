47767dc5b43f580c8931028fec39248116fb5b4c
Quartz-DV <238630229+Quartz-DV@users.noreply.github.com>
2026-04-20 08:58:11 +0200

    visual studio domein aanpassing

--- ProjectBeheeder_/ProjectBeheederBL/Domein/Project.cs
+++ ProjectBeheeder_/ProjectBeheederBL/Domein/Project.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectBeheerderBL.Beheerder {
+namespace ProjectBeheerderBL.Domein {
     internal class Project {
     }
 }
--- ProjectBeheeder_/ProjectBeheederBL/Domein/ProjectPartner.cs
+++ ProjectBeheeder_/ProjectBeheederBL/Domein/ProjectPartner.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectBeheerderBL.Beheerder {
-    internal class ProjectFilter {
+namespace ProjectBeheerderBL.Domein {
+    internal class ProjectPartner {
     }
 }
