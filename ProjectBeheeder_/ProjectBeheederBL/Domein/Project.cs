3e711d7b1e2af5a80a7ebba035d967b5bec4ae46
Quartz-DV <238630229+Quartz-DV@users.noreply.github.com>
2026-04-20 12:48:03 +0200

    lala

--- ProjectBeheeder_/ProjectBeheederBL/Domein/Project.cs
+++ ProjectBeheeder_/ProjectBeheederBL/Domein/Project.cs
@@ -15,6 +15,7 @@ namespace ProjectBeheerderBL.Domein
         public string Titel { get; set; }
         public ProjectStatus Status { get; set; }
         public DateTime StartDatum { get; set; }
+        public string Beschrijving { get; set; }
         public Locatie Locatie { get; set; }
         public List<ProjectPartner> Partners { get; set; } = new List<ProjectPartner>();
         public List<ProjectDetail> Details { get; set; } = new List<ProjectDetail>();
