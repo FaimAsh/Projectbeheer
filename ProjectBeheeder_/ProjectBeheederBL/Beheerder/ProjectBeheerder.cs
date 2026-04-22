1288899887078bc9e75eb69d27eec3ee8ac7306b
Gheron-Doe <182628283+Gheron-Doe@users.noreply.github.com>
2026-04-22 14:46:16 +0200

    ç

--- ProjectBeheeder_/ProjectBeheederBL/Beheerder/ProjectBeheerder.cs
+++ ProjectBeheeder_/ProjectBeheederBL/Beheerder/ProjectBeheerder.cs
@@ -24,16 +24,6 @@ namespace ProjectBeheerderBL.Beheerder
 
             _writer.Write(path, projecten);
         }
-        public void ExportCsv(List<Project> projecten, string fileName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ExportPdf(List<Project> projecten, string fileName)
-        {
-            throw new NotImplementedException();
-        }
-
 
         public ProjectBeheerder(IProjectRepository repository)
         {
@@ -49,23 +39,12 @@ namespace ProjectBeheerderBL.Beheerder
         public void UpdateProject(Project p) =>    _repository.UpdateProject(p);
         public void DeleteProject(Project p) =>    _repository.ProjectVerwijderen(p);
         public void DeletePartner(Partner p) =>    _repository.PartnerVerwijderen(p);
-<<<<<<< HEAD
+
         public void GetPartners(Partner p) => _repository.GeefPartners();
        
-=======
+
         public void AddPartner(int id,ProjectPartner NieuwePartner) => _repository.PartnerAanmaken(id,NieuwePartner);
         public List<Partner> GivePartner() => _repository.GeefPartners();
-        public void ExportCsv(List<Project> projecten, string path)
-        {
-            if (_writer == null) throw new InvalidOperationException("Geen exporter geconfigureerd.");
-            _writer.Write(path, projecten);
-        }
 
-        public void ExportPdf(List<Project> projecten, string path)
-        {
-            if (_writer == null) throw new InvalidOperationException("Geen exporter geconfigureerd.");
-            _writer.Write(path, projecten);
-        }
->>>>>>> 7c7e7b46b821030ca40c02fdcaf4f94802adb488
     }
 }
